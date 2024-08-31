57157166cf769a2229a2c90082569121a7cec3c7
mwiza0979001175@gmail.com <mwiza0979001175@gmail.com>
2024-08-31 17:15:31 +0200

    good so far, can pull

--- careann-frontend/src/App.js
+++ careann-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import CareSeekerDashboard from './components/Dashboard/CareSeekerDashboard';
